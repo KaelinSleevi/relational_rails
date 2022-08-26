30375500951cfdbe1dc6f9ae977a1f92326dc435
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-26 17:28:12 -0600

    fix foreign key and database

--- db/migrate/20220826012512_add_equipment_to_shops.rb
+++ db/migrate/20220826012512_add_equipment_to_shops.rb
@@ -1,5 +1,5 @@
 class AddEquipmentToShops < ActiveRecord::Migration[5.2]
   def change
-    add_reference :equipment, :shops, foreign_key: true
+    add_reference :equipment, :shop, foreign_key: true
   end
 end
