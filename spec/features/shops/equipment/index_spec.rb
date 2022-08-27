30ac7be4d6ec5cfa9b5b14cd346e933a343c73c1
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-26 21:40:15 -0600

    add finished card 5 for parent child index

--- spec/features/shops/equipment/index_spec.rb
+++ spec/features/shops/equipment/index_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe 'Shops Equipment Index' do
         @evolution = Shop.create!(name: 'Evolution', ratings: 4.25, is_open: false)
         @skisplus = Shop.create!(name: 'Skis Plus', ratings: 3.7, is_open: false)
 
-        @equipment = @evolution.equipments.create!(brand: "Black Crows Skis", price: 667.28, in_stock:  false, shop_id: 1)
+        @equipment = @evolution.equipments.create!(brand: "Black Crows Skis", price: 667.28, in_stock:  true, shop_id: 1)
         @equipment1 = @skisplus.equipments.create!(brand: "Atomic Maven Skis", price: 299.97, in_stock: false, shop_id: 2)
-        @equipment2 = @evolution.equipments.create!(brand: "Giro Ceva Helmet", price: 74.99, in_stock: false, shop_id: 1)
+        @equipment2 = @evolution.equipments.create!(brand: "Giro Ceva Helmet", price: 74.99, in_stock: true, shop_id: 1)
         @equipment3 = @skisplus.equipments.create!(brand: "Smith Holt Helmet", price: 48.99, in_stock: false, shop_id: 2)
-        @equipment4 = @evolution.equipments.create!(brand: "Black Crows Poles", price: 44.99, in_stock: false, shop_id: 1)
+        @equipment4 = @evolution.equipments.create!(brand: "Black Crows Poles", price: 44.99, in_stock: true, shop_id: 1)
         @equipment5 = @skisplus.equipments.create!(brand: "Volkl Poles", price: 49.0, in_stock: false, shop_id: 2)
 
     end
@@ -25,7 +25,7 @@ RSpec.describe 'Shops Equipment Index' do
 
         expect(page).to have_content(@evolution.name)
         expect(page).to_not have_content(@skisplus.name)
-        require "pry"; binding.pry
+
     end
 
     it 'shows the brands for each piece of equipment' do
@@ -38,7 +38,7 @@ RSpec.describe 'Shops Equipment Index' do
     it 'shows the price for each piece of equipment' do
 
         visit "/shops/#{@evolution.id}/equipments"
-        
+        save_and_open_page
         expect(page).to have_content(@equipment.price)
         expect(page).to_not have_content(@equipment1.price)
     end
@@ -47,8 +47,8 @@ RSpec.describe 'Shops Equipment Index' do
         
         visit "/shops/#{@evolution.id}/equipments"
         
-        expect(page).to have_content(@equipment.in_stock)
-        expect(page).to_not have_content(@equipment1.in_stock)
+        expect(page).to have_content("Stock? In Stock")
+        expect(page).to_not have_content("Stock? Out of Stock")
     end
 
     xit 'links to each piece of equipments show page' do
