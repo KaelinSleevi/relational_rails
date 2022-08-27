146213d396cecb909fa50e7f6a765ecb291852a2
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-26 22:17:41 -0600

    add finished user story 6. Order by creation parent index

--- spec/features/shops/index_spec.rb
+++ spec/features/shops/index_spec.rb
@@ -8,9 +8,17 @@ RSpec.describe "Shops index page", type: :feature do
                 shop_2 = Shop.create!(name: 'Skis Plus', ratings: 3.7, is_open: false)
                 
                 visit '/shops'
-                # save_and_open_page
 
                 expect(page).to have_content(shop_1.name)
+            end
+
+            it 'I can see the time and date of creation' do
+                shop_1 = Shop.create!(name: 'Evolution', ratings: 4.25, is_open: false)
+                shop_2 = Shop.create!(name: 'Skis Plus', ratings: 3.7, is_open: false)
+                
+                visit '/shops'
+
+                expect(page).to have_content(shop_1.created_at)
             end
         end
     end
