588b07493b7fff7216fe080cd5cbc744704e0923
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-27 19:05:55 -0600

    add progress user story 13. Unfinished,

--- app/controllers/shop_equipments_controller.rb
+++ app/controllers/shop_equipments_controller.rb
@@ -1,4 +1,27 @@
 class ShopEquipmentsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+        @evolution = Shop.find(params[:id])
+
+        @equipment = @evolution.equipments.create!({
+            brand: params[:equipment][:brand],
+            price: params[:equipment][:price],
+            in_stock: params[:equipment][:in_stock]
+            })
+            
+            @equipment.save
+            
+            redirect_to "/shops/#{@evolution.id}/equipments/"
+    end
+
+    def edit
+        @evolution = Shop.find(params[:id])
+    end
+
     def index
         @evolution = Shop.find(params[:shop_id])
     end
